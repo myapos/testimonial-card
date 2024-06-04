ed976ec0cae5a012ecf88c0f2ab95b523d324836
myapos <myapos@gmail.com>
2024-06-04 14:13:08 +0300

    added basic vue setup with basic vue components

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <app-testimonial />
+    <!-- <blog-card
+      :imgSrc="imgSrc"
+      alt="top 5 living room inspirations"
+      header="Top 5 living room inspirations"
+      tag="Interior"
+      description="Curated vibrants Colors for your living, make it pop & calm in the same time."
+    />
+
+    <card-list v-if="showCardList" /> -->
+  </main>
+  <app-footer author="Myron Apostolakis" username="myapos-598" />
+</template>
+
+<script lang="ts">
+import AppFooter from './components/AppFooter.vue'
+import AppTestimonial from './components/TestimonialAvatar/AppTestimonial.vue'
+// import BlogCard from './components/BlogCard.vue'
+// import CardList from './components/CardList.vue'
+// import imgSrc from './assets/spacejoy-YqFz7UMm8qE-unsplash.jpg'
+
+export default {
+  name: 'App',
+  components: { AppFooter, AppTestimonial },
+  data() {
+    // set showCardList to true to test a card list approach
+    return { showCardList: false }
+  }
+}
+</script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="credits fixed bottom-6 left-1/2 translate-x-[-50%] text-xs text-[--footer-text-color]
+      text-center"
+    data-gfe-screenshot-exclude="true"
+  >
+    A challenge by
+    <a href="https://www.greatfrontend.com/projects?ref=challenges" target="_blank"
+      >GreatFrontEnd Projects</a
+    >. Built by
+    <a
+      :href="`https://www.greatfrontend.com/projects/u/${username}`"
+      target="_blank"
+      class="font-bold text-black"
+      >{{ author }}</a
+    >.
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'AppFooter',
+  props: {
+    author: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
